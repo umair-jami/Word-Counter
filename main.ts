7d646b263aa113ec0a8ae9b39ae61fd2fe2f3df7
Umair_Jami <lalaumair92@gmail.com>
2024-04-17 22:28:39 +0500

    Updated README.md

---
+++ main.ts
@@ -0,0 +1,21 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+//  Declare a constant variable and assign it to the result of inquirer.prompt function
+
+const answers: {
+  sentence: string;
+} = await inquirer.prompt([
+  {
+    name: "sentence",
+    type: "input",
+    message: "Enter your Sentence to count the number of words:",
+  },
+]);
+
+const words = answers.sentence.trim().split(" ");
+//print the array of words to the console
+console.log(words);
+
+//print the length of the array
+console.log(`you sentence word count is ${words.length}`);
